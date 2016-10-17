ebb855e6e6bc5619bb521f6be2d2c1fa796ab733
Chukwuerika Dike <chukwuerika.dike@andela.com>
2016-10-17 14:31:21 +0100

    [Chore] Write tests for the app

--- search_books/app/tests/test_models.py
+++ search_books/app/tests/test_models.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+
+class BookTestSuite(TestCase):
+    '''Tests for the Book model'''
+    def setUp(self):
+        self.book = Book.objects.create(
+            title='Knightmare Academy',
+            category='thriller',
+            date_published=timezone.now()
+        )
+
+    def test_book_model(self):
+        book = Book.objects.all()[0]
+        self.assertEqual(str(book), self.book.title)
--- search_books/app/tests/test_views.py
+++ search_books/app/tests/test_views.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.http import QueryDict
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your tests here.
+
+class SearchViewTestSuite(TestCase):
+    '''Tests for search view and functionality'''
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title='Knightmare Academy',
+            category='thriller',
+            date_published=timezone.now()
+        )
+        self.client = Client()
+
+    def test_get_search_view(self):
+        url = reverse('search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_for_book(self):
+        query_dict = QueryDict('', mutable=True)
+        query_dict.update({'q': 'academy'})
+        url = '{base_url}?{query_string}'.format(
+            base_url=reverse('search'),
+            query_string=query_dict.urlencode()
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Knightmare Academy', response.content)
